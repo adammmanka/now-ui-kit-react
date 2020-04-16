e4c9aff91437e6970520f586d55006efd49f2174
Adam M Manka <adammmanka@gmail.com>
2020-04-15 20:06:54 -0400

    changed profilepage.js to make it index also deleted junk

--- src/index.js
+++ src/index.js
@@ -27,9 +27,6 @@ import "assets/demo/nucleo-icons-page-styles.css";
 // pages for this kit
 import Index from "views/Index.js";
 import NucleoIcons from "views/NucleoIcons.js";
-import LoginPage from "views/examples/LoginPage.js";
-import LandingPage from "views/examples/LandingPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -40,15 +37,6 @@ ReactDOM.render(
           path="/nucleo-icons"
           render={props => <NucleoIcons {...props} />}
         />
-        <Route
-          path="/landing-page"
-          render={props => <LandingPage {...props} />}
-        />
-        <Route
-          path="/profile-page"
-          render={props => <ProfilePage {...props} />}
-        />
-        <Route path="/login-page" render={props => <LoginPage {...props} />} />
         <Redirect to="/index" />
         <Redirect from="/" to="/index" />
       </Switch>
